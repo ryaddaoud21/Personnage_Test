6a655c812e75e0a624e9d4a0fb138b418eb96f06
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-03-22 15:24:15 +0100

    Feat[Add-Text.1]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let's test it!");
+        System.out.println("Let's test it again!");
     }
 }
