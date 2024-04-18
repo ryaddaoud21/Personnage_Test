891d91681a2802c619c3424c7ae96c69194b49da
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-18 09:45:56 +0200

    Feat[Edit-yml.13]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let's test it again and again!");
+        System.out.println("Let's testtest it again and again!");
     }
 }
